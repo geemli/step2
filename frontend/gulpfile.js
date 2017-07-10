7053a1c7745571775cdd212e3295e843a59fdfd4
Dzmitry Norka <d.norka@sam-solutions.com>
2017-07-10 12:37:45 +0300

    'gulp-inject'

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,24 +1,22 @@
-// Include gulp
 var gulp = require('gulp');
- // Include plugins
+var inject = require('gulp-inject');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var mainBowerFiles = require('gulp-main-bower-files');
 
-gulp.task('uglify', function(){
-    return gulp.src('./bower.json')
+
+
+gulp.task("uglify", function(){
+	var target = gulp.src('./public/index.html');
+	var sources = gulp.src('./public/dest/app.min.js');
+	
+	return gulp.src('./bower.json')
         .pipe(mainBowerFiles( ))
         .pipe(uglify())
-		.pipe(concat('vendor.js'))
-        .pipe(gulp.dest('public/dist'));
+        .pipe(concat("app.min.js"))
+        .pipe(gulp.dest('./public/dest'))
+		.pipe(target.pipe(inject(sources)))
+			.pipe(gulp.dest('./public'));
 });
 
-
- // Concatenate JS Files
-gulp.task('scripts', function() {
-    return gulp.src('bower_components/angular/*.js')
-      .pipe(concat('helloworld.js'))
-      .pipe(gulp.dest('build/js'));
-});
- // Default Task
 gulp.task('default', ['uglify']);
