187cf7781a423826431b5a33bc19544548f859dd
Dzmitry Norka <d.norka@sam-solutions.com>
2017-07-21 14:48:30 +0300

    ''

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,28 +3,43 @@ var inject = require('gulp-inject');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var mainBowerFiles = require('gulp-main-bower-files');
+var del = require('del');
+var es = require('event-stream');
 
-
-
-gulp.task("uglify", function(){
-	var target = gulp.src('./src/index.html');
-	var sources = gulp.src('./public/app.min.js');
-	
-	return gulp.src('./bower.json')
+gulp.task('pack-vendor-js', ['clean'], function() {
+    return gulp.src('./bower.json')
         .pipe(mainBowerFiles( ))
-        .pipe(uglify())
-        .pipe(concat("app.min.js"))
-        .pipe(gulp.dest('./public'))
-		.pipe(target.pipe(inject(sources)))
-			.pipe(gulp.dest('./public'));
+        .pipe(concat('vendors.js'))
+        .pipe(gulp.dest('./public'));
 });
 
-gulp.task("uglify2", function(){
-	return gulp.src('./bower.json')
-        .pipe(mainBowerFiles( ))
+gulp.task('pack-my-js', ['clean'], function() {
+    return gulp.src(['./src/*.js'])
+        .pipe(concat('app.js'))
         .pipe(uglify())
-        .pipe(concat("app2.min.js"))
-        .pipe(gulp.dest('./public'))
+        .pipe(gulp.dest('./public'));
 });
 
-gulp.task('default', ['uglify', 'uglify2']);
+gulp.task('pack-css', ['clean'], function() {
+    return gulp.src('./src/*.css')
+        .pipe(concat('stylesheet.css'))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('copy', ['pack-vendor-js', 'pack-my-js', 'pack-css'], function() {
+    return gulp.src(['./src/*.html', '!./src/index.html'])
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('default', ['inject']);
+
+gulp.task('clean',function() {
+    del(['public']);
+});
+
+gulp.task('inject', ['copy'],  function () {
+    return gulp.src('./src/index.html')
+        .pipe(inject(gulp.src(['./public/*.js'])))
+        .pipe(inject(gulp.src(['./public/*.css'])))
+        .pipe(gulp.dest('./public'));
+});
